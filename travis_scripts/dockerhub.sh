a7192d198a3b963bf8198363de51626a882cd547
Benjamin Wulff <benjamin.wulff.de@ieee.org>
2016-09-20 12:01:37 +0200

    tiny update

--- travis_scripts/dockerhub.sh
+++ travis_scripts/dockerhub.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+echo $DOCKER_USERNAME
+echo $DOCKER_PASSWORD
 docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker build -t slidewiki/userservice ./
 docker push slidewiki/userservice
