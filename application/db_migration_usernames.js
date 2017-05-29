dab7c59ae88edcd2e47e73e5068f7c538f035d72
Kurt Junghanns <tboonx@googlemail.com>
2017-05-29 12:31:03 +0200

    [SWIK-1280_username_case_insensitive] MAde migration script exiting itself.

--- application/db_migration_usernames.js
+++ application/db_migration_usernames.js
@@ -69,9 +69,10 @@ return helper.connectToDatabase()
                     return helper.connectToDatabase()
                       .then((db) => db.collection('users_migrated'))
                       .then((coll) => coll.aggregate([{ $replaceRoot: { newRoot: '$value' } },{ $out: 'users' }]))
-                      .then((collection) => collection.toArray())
+                      // .then((collection) => collection.toArray())
                       .then((result) => {
-                        console.log('Finished!!! Got results from aggregate:', result.length);
+                        console.log('Finished!!! Got results from aggregate:', result);
+                        process.exit(0);
                         return true;
                       });
                   });
@@ -81,70 +82,6 @@ return helper.connectToDatabase()
   })
   .catch((error) => {
     console.log('Error:', error);
+    process.exit(0);
+    return;
   });
-
-/*
-db.users_duplications.drop()
-db.users_migrated.drop()
-db.runCommand(   {     mapReduce: 'users',     map: function() {       emit(this.username.toLowerCase().replace(/\s/g,''), this);     },     reduce: function(key, values) {        return {         count: values.length,         username: key,         users: values.reduce(function(prev, curr) {           if (curr.count)             prev.concat(curr.users);           else             prev.push(curr);           return prev;         }, [])       };     },     out: 'users_duplications'   } )
-db.runCommand(   {     mapReduce: 'users_duplications',     map: function() {       if (this.value.count === undefined) {         this.value.username = this.value.username.replace(/\s/g,'_');         emit(this.value._id, this.value);       }       else {         let i = 0;         this.value.users.forEach(function(user) {           let username = user.username.replace(/\s/g,'_');           for (let j = 0; j < i; j++) {             username = username+'ish';           }           i++;           user.username = username;           emit(user._id, user);         });       }     },     reduce: function(key, values) {        return values;     },     out: 'users_migrated'   } )
-db.users.drop()
-db.users_migrated.aggregate([{ $replaceRoot: { newRoot: '$value' } },{ $out: 'users' }])
-*/
-
-
-/*
-db.users.find({username: {$regex: /\s/g}})
-db.runCommand(
-  {
-    mapReduce: 'users',
-    map: function() {
-      emit(this.username.toLowerCase().replace(/\s/g,''), this);
-    },
-    reduce: function(key, values) {
-
-      return {
-        count: values.length,
-        username: key,
-        users: values.reduce(function(prev, curr) {
-          if (curr.count)
-            prev.concat(curr.users);
-          else
-            prev.push(curr);
-          return prev;
-        }, [])
-      };
-    },
-    out: 'users_duplications'
-  }
-)
-
-db.runCommand(
-  {
-    mapReduce: 'users_duplications',
-    map: function() {
-      if (this.value.count === undefined) {
-        this.value.username = this.value.username.replace(/\s/g,'_');
-        emit(this.value._id, this.value);
-      }
-      else {
-        let i = 0;
-        this.value.users.forEach(function(user) {
-          let username = user.username.replace(/\s/g,'_');
-          for (let j = 0; j < i; j++) {
-            username = username+'ish';
-          }
-          i++;
-          user.username = username;
-          emit(user._id, user);
-        });
-      }
-    },
-    reduce: function(key, values) {
-
-      return values;
-    },
-    out: 'users_migrated'
-  }
-)
-*/
