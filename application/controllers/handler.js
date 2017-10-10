e4c45ffbbd4cc455555d0648b9c5d078178415ae
Kurt Junghanns <tboonx@googlemail.com>
2017-10-10 15:46:29 +0200

    [SWIK-1731_search_user_search_in_all_attributes] Changed that not only the username is considered while searching

--- application/controllers/handler.js
+++ application/controllers/handler.js
@@ -537,27 +537,20 @@ module.exports = {
   },
 
   searchUser: (req, res) => {
-    let username = decodeURI(req.params.username).replace(/\s/g,'');
+    let term = decodeURI(req.params.term);
 
-    let schema = Joi.string().regex(/^[\w\-.~]*$/);
-    let valid = Joi.validate(username, schema);
-
-    if (valid.error === null) {
-      username = valid.value + '+';
-    }
-    else {
-      if (username === '') {
-        //search random users - a* matches everyone
-        username = 'a*';
-      }
-      else  {
-        console.log('username is invalid:', username, valid.error);
-        return res({success: false, results: []});
-      }
+    if (term === undefined || term === null || term === '') {
+      term = '\w*';
     }
 
     const query = {
-      username: new RegExp(username, 'i'),
+      $or: [
+        {username: new RegExp(term, 'i')},
+        {email: new RegExp(term, 'i')},
+        {forename: new RegExp(term, 'i')},
+        {surname: new RegExp(term, 'i')},
+        {organization: new RegExp(term, 'i')}
+      ],
       deactivated: {
         $not: {
           $eq: true
@@ -575,14 +568,10 @@ module.exports = {
       }
     };
 
-    if (username === undefined || username === null || username === '') {
-      query.username = new RegExp('\w*', 'i');
-    }
-
-    // console.log('query:', query);
+    console.log('query:', query);
 
     return userCtrl.find(query)
-      .then((cursor1) => cursor1.project({username: 1, _id: 1, picture: 1, country: 1, organization: 1}))
+      .then((cursor1) => cursor1.project({username: 1, _id: 1, country: 1, organization: 1}))
       .then((cursor2) => cursor2.limit(8))
       .then((cursor3) => cursor3.toArray())
       .then((array) => {
