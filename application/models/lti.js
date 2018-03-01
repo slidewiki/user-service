f53fcf68211e2673cb6d380e95e5df4d305f4813
urashidmir <umar.mir@open.ac.uk>
2018-03-01 15:36:23 +0000

    LTI Handler added

---
+++ application/models/lti.js
@@ -0,0 +1,34 @@
+'use strict';
+
+//require
+let Ajv = require('ajv');
+let ajv = Ajv({
+  verbose: true,
+  allErrors: true
+  //v5: true  //enable v5 proposal of JSON-schema standard
+}); // options can be passed, e.g. {allErrors: true}
+
+//build schema
+const objectid = {
+  type: 'string',
+  maxLength: 24,
+  minLength: 24
+};
+const ltiKeys = {
+  type: 'object',
+  properties: {
+    _id: {
+      type: 'integer'
+    },
+    key: {
+      type: 'string'
+    },
+    secret: {
+      type: 'string'
+    }
+  },
+  required: ['key', 'token', 'token_creation', 'id']
+};
+
+//export
+module.exports = ajv.compile(ltiKeys);
