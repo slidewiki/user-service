f3319ba7c964e356554800f7011d22fe68036938
Roy Meissner <meissner@informatik.uni-leipzig.de>
2018-03-23 12:56:38 +0100

    changed the email text

--- application/controllers/handler.js
+++ application/controllers/handler.js
@@ -1228,24 +1228,25 @@ module.exports = {
       case 2: { //video recorded
         //check data values
         if (!req.payload.data.fileName || !req.payload.data.deck || !req.payload.data.creationDate) {
-          return res(boom.badRequest('payload.data was wrong'));
+          return res(boom.badRequest('payload.data does not include all needed fields'));
         }
 
         return userCtrl.find({_id: req.params.id})
           .then((cursor) => cursor.toArray())
           .then((array) => {
             if (array.length !== 1) {
-              return res(boom.notFound());
+              return res(boom.notFound('User does not exist'));
             }
 
             let email = array[0].email;
+            let videoURL = require('../configs/microservices').file.uri + '/video/' + req.payload.data.fileName;
 
             let connectionPromise = util.sendEMail(email,
-              'New video about your session of deck ' + req.payload.data.deck + (req.payload.data.revision) ? '-'+req.payload.data.revision : '',
-              'Dear SlideWiki user ' + array[0].username + ',\n\nYou\'ve held a live session about ' + req.payload.data.deck + (req.payload.data.revision) ? '-'+req.payload.data.revision : '' + ' that ended at about ' + req.payload.data.creationDate + '. Our system finished to create a video of this session. You can view or download the video at: ' + require('../configs/microservices').file.uri + '/video/' + req.payload.data.fileName + '\nFeel free to use the video for whatever reason you like to.\n\nThanks,\nthe SlideWiki Team');
+              'New video about your live session of deck ' + req.payload.data.deck + (req.payload.data.revision) ? '-'+req.payload.data.revision : '',
+              'Dear ' + array[0].username + ',\n\nwe have finshed the video about your live session of deck ' + req.payload.data.deck + (req.payload.data.revision) ? '-'+req.payload.data.revision : '' + ' that ended at ' + req.payload.data.creationDate + '. Feel free to watch the video at: <a href="' + videoURL + '">' + videoURL + '</a>. if you want to download it, open the mentioned link, right click on the video and select "Save as ...". Please keep in mind we delete videos after eight weeks.\nFeel free to use, share and modify the video according to the license <a href="https://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International (CC BY 4.0)</a>.\n\nYour SlideWiki Team');
 
             return connectionPromise
-              .then((data) => {
+              .then(() => {
                 return res();
               })
               .catch((error) => {
