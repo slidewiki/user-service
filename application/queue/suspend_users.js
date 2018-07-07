9fd0c94f0a0aafba53e03260f133a4ee1b7c9f60
Kostis Pristouris <kprist@gmail.com>
2018-07-07 03:21:44 +0300

    Remove requirement for JWT when running suspend_users.js script
    Also add helper scripts to mass importing deckids owned by users to
    be suspended.

--- application/queue/suspend_users.js
+++ application/queue/suspend_users.js
@@ -1,5 +1,3 @@
-/*global emit*/
-/*eslint quotes: "warn"*/
 'use strict';
 
 console.log('This script will read userids from the given file and suspend them one after another.');
@@ -13,10 +11,15 @@ const config = require('../configuration'),
 
 let deckidsToUserids = {};
 
-if (process.argv[2] === undefined) {
-  console.log('Please provide a JWT!');
-  return;
-}
+// generate a `isReviewer` jwt the deck service will accept  
+const jwt = require('jsonwebtoken');
+const adminToken = jwt.sign({
+  userid: -1,
+  username: 'system',
+  isReviewer: true,
+}, config.JWT.SERIAL, {
+  algorithm: config.JWT.ALGORITHM,
+});
 
 console.log('First the userids are read.');
 let linePromises = [];
@@ -79,16 +82,16 @@ function suspendUsers() {
   console.log('Finished reading userids.');
   console.log('Count of correct userids read: ', Object.keys(deckidsToUserids).length);
 
-  console.log("\nNow suspending users and decks");
+  console.log('\nNow suspending users and decks');
   async.eachOfSeries(Object.keys(deckidsToUserids), (ui, key, callback) => {
     let userid = parseInt(ui);
 
-    console.log("\nNow its userid ", userid);
+    console.log('\nNow its userid ', userid);
 
     //remove user from usergroups
     helper.connectToDatabase()
       .then((dbconn) => dbconn.collection('usergroups'))
-      .then((collection) => collection.update({"members.userid": userid}, {
+      .then((collection) => collection.update({'members.userid': userid}, {
         $pull: {
           members: {
             userid: userid
@@ -106,7 +109,7 @@ function suspendUsers() {
     //delete usergroups where user is creator
     helper.connectToDatabase()
       .then((dbconn) => dbconn.collection('usergroups'))
-      .then((collection) => collection.deleteMany({$or: [{"creator.userid": userid}, {creator: userid}]}))
+      .then((collection) => collection.deleteMany({$or: [{'creator.userid': userid}, {creator: userid}]}))
       .then((result) => {
         if (result.result.ok !== 1) {
           console.log('Could not remove usergroup:', result);
@@ -168,7 +171,7 @@ function suspendUsers() {
                 console.log('userid '+userid+', root decks: ', response);
 
                 async.eachOfSeries(response, (deck, key, callback2) => {
-                  archiveDeck(deck._id, process.argv[2], 'spam')
+                  archiveDeck(deck._id, adminToken, 'spam')
                     .then((deckid2) => {
                       deckidsToUserids[userid].push(deckid2);
                       callback2();
@@ -206,12 +209,12 @@ function suspendUsers() {
     let message = '';
     for (let k in deckidsToUserids) {
       let deckids = deckidsToUserids[k].reduce((s, c) => {return s + ', ' + c;}, '');
-      message += k + ' [' + deckids + "], \n";
+      message += k + ' [' + deckids + '], \n';
     }
 
-    console.log("\nHere are all the userids of the suspended users with their suspended decks:\n", message);
+    console.log('\nHere are all the userids of the suspended users with their suspended decks:\n', message);
 
-    console.log("\n\nAt the end, suspended users have to be removed from the queue if they are already there.");
+    console.log('\n\nAt the end, suspended users have to be removed from the queue if they are already there.');
     let query = {$or: Object.keys(deckidsToUserids).reduce((arr, curr) => {
       arr.push({userid: parseInt(curr)});
       return arr;
